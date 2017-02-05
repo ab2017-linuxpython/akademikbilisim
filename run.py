765fdc02fd4d9894ed0fa4b171fada47cb3aef34
Umut Karcı <cediddi@gmail.com>
2017-02-05 11:48:16 +0300

    Signal kütüphanesinin kullanımı örneklendi

--- run.py
+++ run.py
@@ -1,8 +1,34 @@
+import os
 import sys
+import signal
 
 import gozcu
 
+
+def term_handle(sig, stack):
+    print("{} numaralı sinyal alındı".format(sig), file=sys.stderr, flush=True)
+    exit(100 + sig)
+
+
+def int_handle(sig, stack):
+    print("Kardeş yapma.... Yapma bak....", file=sys.stderr, flush=True)
+
+
+def usr1_handle(sig, stack):
+    print("PID: {}".format(os.getpid()), file=sys.stderr, flush=True)
+
+
+def usr2_handle(sig, stack):
+    sys.stdout = open(os.devnull, "w")
+
+
+signal.signal(signal.SIGTERM, term_handle)
+signal.signal(signal.SIGINT, int_handle)
+signal.signal(signal.SIGUSR1, usr1_handle)
+signal.signal(signal.SIGUSR2, usr2_handle)
+
 try:
+    print("PID: {}".format(os.getpid()))
     gozcu.ana_fonksyon()
 except Exception as e:
     print("An error occured:", file=sys.stderr)
